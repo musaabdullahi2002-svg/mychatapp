4782eb5778a91d926d3dff7f8f479979dcb5742b
musaabdullahi2002-svg <musaabdullahi2002-svg>
2025-12-23 15:51:09 +0100

    text

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Routes>
+
+     </Routes>
+    </>
+  )
+}
+
+export default App
